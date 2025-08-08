8c707e421a885e809c953bb16ceeb20d653085c1
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2025-08-08 18:53:13 +0530

    login

--- useradmin/src/app/app.routes.ts
+++ useradmin/src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-	path: '',
+	path: 'default',
 	redirectTo: '/home',
 	pathMatch: 'full'
   },
   {
 	path: 'home',
-	loadComponent: () => import('./home/home').then(m => m.Home)
+	loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+	// Make sure './home/home.component.ts' exists and exports HomeComponent
   }
 ];
 
